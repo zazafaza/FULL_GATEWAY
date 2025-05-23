0e23ee6ea1278a2d84f24cd0391eda48a8c485bd
zazafaza <135862928+zazafaza@users.noreply.github.com>
2025-05-23 03:11:54 +0300

    Delete src directory

--- src/components/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-  return (
-    <div className='text-center p-4'>
-      <h1 className='text-3xl font-bold text-blue-500'>Welcome to RunMaxPay 🚀</h1>
-    </div>
-  );
-}
-export default App;
